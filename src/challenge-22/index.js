6454089a6e1e41cf86fce456673b43766c4f5aa3
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-12-31 14:33:09 -0600

    :white_check_mark: Challenge 22 added

---
+++ src/challenge-22/index.js
@@ -0,0 +1,9 @@
+function checkStepNumbers(systemNames, stepNumbers) {
+  return systemNames.every((e, i) => 
+    stepNumbers[i] < stepNumbers[
+      i + systemNames.slice(i + 1).indexOf(e) + 1
+    ] + !(systemNames.lastIndexOf(e) - i)
+  )
+}
+
+module.exports = checkStepNumbers
