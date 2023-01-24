3ff166a474a101237a3919f7be6632c985ed2dd2
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-01-23 23:23:17 -0600

    🎨 Add Alternatives for challenge 03

--- src/challenge-03/index.js
+++ src/challenge-03/index.js
@@ -13,4 +13,14 @@ function distributeGifts(packOfGifts, reindeers) {
   return maxPacks
 }
 
+function distributeGiftsAlt1(packOfGifts, reindeers) {
+  return (reindeers.join('').length * 2 / packOfGifts.join('').length) << 0
+}
+
+function distributeGiftsAlt2(packOfGifts, reindeers) {
+  let pesoMax = reindeers.join("").length * 2
+  let pesoCaja = packOfGifts.join("").length
+  return (pesoMax / pesoCaja) >> 0
+}
+
 module.exports = distributeGifts
