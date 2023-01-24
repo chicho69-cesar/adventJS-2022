247415221a36efad7ef574f131ff2c7bdfbffbb0
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-01-23 23:23:33 -0600

    🎨 Add Alternatives for challenge 05

--- src/challenge-05/index.js
+++ src/challenge-05/index.js
@@ -29,4 +29,51 @@ function getMaxGifts(giftsCities, maxGifts, maxCities) {
   return sumMax
 }
 
+function getMaxGiftsAlt1(giftsCities, maxGifts, maxCities) { 
+  return Math.max(
+    ...giftsCities
+      .sort((x, y) => y - x)
+      .reduce(
+        (result, _, i) => (
+          i && giftsCities.unshift(giftsCities.pop()),
+          (i = giftsCities
+            .slice(0, maxCities)
+            .reduce((acc, curr) => (acc += curr), 0)),
+          i <= maxGifts && result.push(i),
+          i - giftsCities[maxCities - 1] <= maxGifts &&
+          result.push(i - giftsCities[maxCities - 1]),
+          result
+        ),
+        []
+      ),
+    0
+  );
+}
+
+function getMaxGiftsAlt2(giftsCities, maxGifts, maxCities) {
+  let counter = 0
+  let sum = 0
+
+  return giftsCities.reduce((acc, curr, index) => {
+    if (acc < curr && curr <= maxGifts) acc = curr
+    sum = curr
+    counter++
+    giftsCities.forEach((secondCity, index2) => {
+      if (index2 === index) return acc
+      if (counter < maxCities && ((sum + secondCity) <= maxGifts)) {
+        sum += secondCity
+        counter++
+        if (counter <= maxCities) {
+          if (acc < sum && sum <= maxGifts) {
+            acc = sum
+          }
+        }
+      }
+    })
+    counter = 0
+    return acc
+  }, 0)
+}
+
+
 module.exports = getMaxGifts
