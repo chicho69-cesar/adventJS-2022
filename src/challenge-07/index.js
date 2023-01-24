0ca8be91373dc5c3e979bb4aa30b6dfef701fb0a
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-01-23 23:23:41 -0600

    🎨 Add Alternatives for challenge 07

--- src/challenge-07/index.js
+++ src/challenge-07/index.js
@@ -12,4 +12,12 @@ function getGiftsToRefill(a1, a2, a3) {
   return uniqueGifts
 }
 
+function getGiftsToRefillAlt(a1, a2, a3) {
+  return [
+    ...new Set([ ...a1, ...a2, ...a3 ])
+  ].filter(gift => 
+    a1.includes(gift) + a2.includes(gift) + a3.includes(gift) === 1
+  )
+}
+
 module.exports = getGiftsToRefill
