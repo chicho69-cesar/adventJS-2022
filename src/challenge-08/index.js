1d857633be4d9b1f34618d675eb35393e8f4611a
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-01-23 23:23:48 -0600

    🎨 Add Alternatives for challenge 08

--- src/challenge-08/index.js
+++ src/challenge-08/index.js
@@ -17,4 +17,28 @@ function checkPart(part) {
   return band
 }
 
+function checkPartAlt1(part) {
+  return [ ...part ].some((_, i, arr) => {
+    let w = arr.filter((_, y) => y != i)
+    return w.join("") == w.reverse().join("")
+  })
+}
+
+function checkPartAlt2(part) {
+  let counter = 0
+  const reversedArray = part.split('').reverse()
+  
+  return part.split('').every((el, index, array) => {
+    if (el === reversedArray[index]) return true
+    else if (counter === 0) {
+      array.splice(index, 1)
+      reversedArray.splice(reversedArray.length - 1 - index, 1)
+      reversedArray.splice(index, 1)
+      array.splice(reversedArray.length - 1 - index, 1)
+      counter++
+      return true
+    } else return false
+  })
+}
+
 module.exports = checkPart
