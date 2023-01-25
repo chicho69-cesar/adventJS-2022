e001b2098b36cd8ac29ae77f1e14b0e76e6d4dbc
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-01-24 21:14:53 -0600

    🎨 Add Alternatives for challenge 13

--- src/challenge-13/index.js
+++ src/challenge-13/index.js
@@ -12,4 +12,21 @@ function getFilesToBackup(lastBackup, changes) {
   return changesToBackup.map(c => +c)
 }
 
+function getFilesToBackupAlt1(lastBackup, changes) {
+  return [...new Set([ ...changes
+      .filter(item => item[1] > lastBackup)
+      .map(item => item[0])
+    ].sort((a, b) => a - b)
+  )]
+}
+
+function getFilesToBackupAlt2(lastBackup, changes) {
+  let filtered = changes.filter(x => x[1] > lastBackup)
+  let ids = filtered.map(x => x[0])
+  let sorted = ids.sort((a, b) => a - b)
+  let flated = [ ...new Set(sorted) ]
+
+  return flated
+}
+
 module.exports = getFilesToBackup
