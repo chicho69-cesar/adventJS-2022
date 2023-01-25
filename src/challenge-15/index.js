3830316938c551c5ce199468789397231a20957f
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-01-24 21:15:06 -0600

    🎨 Add Alternatives for challenge 15

--- src/challenge-15/index.js
+++ src/challenge-15/index.js
@@ -22,4 +22,29 @@ function decorateTree(base) {
   return result.reverse()
 }
 
+function decorateTreeAlt(base) {
+  const dict = {
+    "PP": "P",
+    "BB": "B",
+    "RR": "R",
+    "BP": "R",
+    "PB": "R",
+    "BR": "P",
+    "RB": "P",
+    "PR": "B",
+    "RP": "B"
+  }
+
+  base = base.split(" ")
+  let list = new Array(base.length).fill(base)
+  
+  return list.reduce((total, x) =>
+    total.concat(
+      [ new Array(total.at(-1).length - 1).fill("-").map((_, i) => {
+        return dict[total.at(-1).slice(i, i + 2).join("")]
+      }) ]
+    ), [base]
+  ).slice(0, base.length).map(x => x.join(" ")).reverse()
+}
+
 module.exports = decorateTree
