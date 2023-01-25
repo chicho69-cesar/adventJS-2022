c4e14edfacd6ef4049f0fda814670b5c912a75c0
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-01-24 21:15:51 -0600

    🎨 Add Alternatives for challenge 24

--- src/challenge-24/index.js
+++ src/challenge-24/index.js
@@ -43,4 +43,28 @@ function canExit(maze) {
   return canExitNow
 }
 
+function canExitAlt(maze) {
+  function fillPath(line) {
+    return line
+      .join("")
+      .replace(/[S][\sE]/g, "SS")
+      .replace(/[\sE][S]/g, "SS")
+      .split("")
+  }
+  
+  const x = maze[0].length
+  const y = maze.length
+  const area = x * y
+
+  new Array(area).fill('').forEach(() => {
+    maze.map((hor, i) => {
+      maze[i] = fillPath(hor)
+      maze[i] = maze[i]
+        .map((_, j) => fillPath(maze.map(x => x[j]))[i])
+    })
+  })
+
+  return !maze.flat(2).includes("E")
+}
+
 module.exports = canExit
