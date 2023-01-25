b21ad99bd21bf3e6d6efc4c20513bfd2d269d912
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-01-24 21:15:23 -0600

    🎨 Add Alternatives for challenge 18

--- src/challenge-18/index.js
+++ src/challenge-18/index.js
@@ -10,4 +10,16 @@ function dryNumber(dry, numbers) {
   return afected
 }
 
+function dryNumberAlt1(dry, numbers) {
+  return Array.from(Array(numbers).keys())
+    .filter(x => `${ x + 1 }`.split("").includes(`${ dry }`))
+    .map(x => x + 1)
+}
+
+function dryNumberAlt2(dry, numbers) {
+  return [ ...Array(numbers + 1).keys() ]
+    .slice(1)
+    .filter(x => (x + '').includes(dry))
+}
+
 module.exports = dryNumber
