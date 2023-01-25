62ab90ef838e422a2541831b881d054749f983c2
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-01-24 21:15:46 -0600

    🎨 Add Alternatives for challenge 22

--- src/challenge-22/index.js
+++ src/challenge-22/index.js
@@ -6,4 +6,16 @@ function checkStepNumbers(systemNames, stepNumbers) {
   )
 }
 
+function checkStepNumbersAlt(systemNames, stepNumbers) {
+  const baseObject = Object.assign({},
+    ...systemNames.map((name, i) => ({ [name]: [] }))
+  )
+
+  return Object.values(systemNames.reduce((acc, curr, i) => {
+    acc[curr] = [ ...acc[curr], stepNumbers[i] ]
+    return acc
+  }, baseObject))
+    .every((c) => c.slice(1).every((next, i) => next >= c[i]))
+}
+
 module.exports = checkStepNumbers
