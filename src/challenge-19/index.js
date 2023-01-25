a4eba1f498587dd7b0879466582e22bfc0f2c3ef
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-01-24 21:15:29 -0600

    🎨 Add Alternatives for challenge 19

--- src/challenge-19/index.js
+++ src/challenge-19/index.js
@@ -1,11 +1,18 @@
 function sortToys(toys, positions) {
   let map = {}
 
-  for (let i = 0; i < toys.length; i++) {
-    map[positions[i]] = toys[i]
-  }
+  toys.forEach((toy, i) => {
+    map[positions[i]] = toy
+  })
 
   return Object.values(map)
+}
+
+function sortToysAlt1(toys, positions) {
+  return toys.sort((a, b) => 
+    positions[toys.indexOf(a)] -
+    positions[toys.indexOf(b)]
+  )
 }
 
 module.exports = sortToys
