4715f3992d58332262b6ebc9a236201920c675d7
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-01-24 21:14:47 -0600

    🎨 Add Alternatives for challenge 12

--- src/challenge-12/index.js
+++ src/challenge-12/index.js
@@ -4,4 +4,20 @@ function selectSleigh(distance, sleighs) {
   return result ? result.name : null
 }
 
+function selectSleighAlt1(distance, sleighs) {
+  let sleigh = sleighs.filter(x => (20 / x.consumption) >= distance).at(-1)
+  return sleigh ? sleigh.name : null
+}
+
+function selectSleighAlt2(distance, sleighs) {
+  sleighs = sleighs.filter((x) => x.consumption * distance <= 20)
+  sleighs.unshift({ name: null })
+  return sleighs[sleighs.length - 1].name
+}
+
+function selectSleighAlt3(distance, sleighs) {
+  const found = sleighs.reverse().find((x) => x.consumption * distance <= 20)
+  return found ? found.name : null
+}
+
 module.exports = selectSleigh
